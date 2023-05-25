b59570adbe1aa01fc9a91ac5f6765a78a07fe69d
DropDrage <danilov.retar@gmail.com>
2023-05-25 22:44:59 +0300

    Lint Fixes

--- common/build_config_checks/src/main/java/com/dropdrage/common/build_config_checks/SdkVersionChecks.kt
+++ common/build_config_checks/src/main/java/com/dropdrage/common/build_config_checks/SdkVersionChecks.kt
@@ -1,5 +1,7 @@
 package com.dropdrage.common.build_config_checks
 
 import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
 
+@ChecksSdkIntAtLeast(parameter = 0)
 fun isSdkVersionGreaterOrEquals(version: Int) = Build.VERSION.SDK_INT >= version
